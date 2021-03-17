f6f4f4b1fe611ec183320a6245182df1897f7909
SLeitgeb <leitgeb.simon@gmail.com>
2021-03-17 18:10:34 +0100

    Add missing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HslayersModule } from 'hslayers-ng';
+import { MainService } from '../main.service';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HslayersModule
   ],
-  providers: [],
+  providers: [
+    MainService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
