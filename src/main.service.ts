485ef51872310933730c409d5ca3f9d82035f728
SLeitgeb <leitgeb.simon@gmail.com>
2021-03-18 17:49:14 +0100

    Update application config

--- src/main.service.ts
+++ src/main.service.ts
@@ -9,9 +9,73 @@ import { transform } from 'ol/proj';
 
 @Injectable({providedIn: 'root'})
 export class MainService {
+  layers: any[];
+
   constructor(
     private HsConfig: HsConfig
   ) { 
+    this.layers = [
+      new Tile({
+        source: new XYZ({
+          attributions: ['Maps <a href="http://www.thunderforest.com">Thunderforest</a>', 'Data <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'],
+          url: 'https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=820bd076452548f894e0059944475cb9',
+        }),
+        title: 'Base layer',
+        base: true
+      }),
+      new Vector({
+        title: 'Regionální speciality',
+        source: new VectorSource({
+          format: new GeoJSON(),
+          url: 'https://db.atlasbestpractices.com/project-geo-json/3/',
+        }),
+        // style: styleFunction,
+        // selectedStyle: highlightedStyleFunction,
+        // highlightedStyle: highlightedStyleFunction,
+        // featureURI: 'bp_uri',
+        // ordering: {
+        //   primary: 'position',
+        //   secondary: 'bp_id',
+        //   defaultReverse: ['position', 'bp_id'],
+        // },
+        // hsFilters: [
+        //   {
+        //     title: "Vyhledávání",
+        //     valueFields: ["name", "description"],
+        //     type: {
+        //       type: "fulltext",
+        //     },
+        //     idField: "bp_id",
+        //     suggestions: false
+        //   },
+        //   {
+        //     title: 'Kategorie',
+        //     valueField: 'tags',
+        //     type: {
+        //       type: 'arrayset',
+        //       parameters: 'or',
+        //     },
+        //     options: {
+        //       unselectText: 'Zrušit výběr',
+        //       selectText: 'Vybrat vše',
+        //     },
+        //     selected: undefined,
+        //     values: [],
+        //     gatherValues: true
+        //   },
+        //   {
+        //     title: 'E-shop',
+        //     valueField: 'eshop_connect',
+        //     type: {
+        //       type: 'compare',
+        //       parameters: 'neq',
+        //     },
+        //     selected: false,
+        //     value: '',
+        //   },
+        // ]
+      })
+    ]
   }
 
   init(): void {
@@ -22,74 +86,8 @@ export class MainService {
         multi: false
       },
       queryPoint: 'hidden',
-      default_layers: [
-        new Tile({
-          source: new XYZ({
-            attributions: ['Maps <a href="http://www.thunderforest.com">Thunderforest</a>', 'Data <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'],
-            url: 'https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=820bd076452548f894e0059944475cb9',
-          }),
-          title: 'Base layer',
-          base: true
-        }),
-        new Vector({
-          title: 'Regionální speciality',
-          source: new VectorSource({
-            format: new GeoJSON(),
-            url: 'https://db.atlasbestpractices.com/project-geo-json/3/',
-          }),
-          // style: styleFunction,
-          // selectedStyle: highlightedStyleFunction,
-          // highlightedStyle: highlightedStyleFunction,
-          // featureURI: 'bp_uri',
-          // ordering: {
-          //   primary: 'position',
-          //   secondary: 'bp_id',
-          //   defaultReverse: ['position', 'bp_id'],
-          // },
-          // hsFilters: [
-          //   {
-          //     title: "Vyhledávání",
-          //     valueFields: ["name", "description"],
-          //     type: {
-          //       type: "fulltext",
-          //     },
-          //     idField: "bp_id",
-          //     suggestions: false
-          //   },
-          //   {
-          //     title: 'Kategorie',
-          //     valueField: 'tags',
-          //     type: {
-          //       type: 'arrayset',
-          //       parameters: 'or',
-          //     },
-          //     options: {
-          //       unselectText: 'Zrušit výběr',
-          //       selectText: 'Vybrat vše',
-          //     },
-          //     selected: undefined,
-          //     values: [],
-          //     gatherValues: true
-          //   },
-          //   {
-          //     title: 'E-shop',
-          //     valueField: 'eshop_connect',
-          //     type: {
-          //       type: 'compare',
-          //       parameters: 'neq',
-          //     },
-          //     selected: false,
-          //     value: '',
-          //   },
-          // ]
-        })
-      ],
-      project_name: 'erra/map',
-      default_view: new View({
-        center: [1452777.8396518824, 6355469.183800302],
-        zoom: 10,
-        units: "m"
-      }),
+      default_layers: this.layers,
+      layersInFeatureTable: [this.layers[1]],
       datasources: [],
       hostname: {
         "default": {
@@ -110,18 +108,6 @@ export class MainService {
         draw: false,
         sensors: false,
         feature_crossfilter: false
-      },
-      layerTooltipDelay: 0,
-      translationOverrides: {
-        "en": {
-          LAYERMANAGER: {
-            'conditions': 'Related to environmental variables',
-            'yield': 'Related to the length of (optimal) growing period of a crop',
-            "mapContent": "Agro-climatic factors",
-          },
-          LAYERS: {
-          }
-        }
       }
     });
   }
